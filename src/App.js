50c514f809ccb830338811a2d45a3a94cecf4f03
mingray <mingray_hsu@perfectcorp.com>
2019-11-19 12:07:48 +0800

    TEST

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://www.dcard.tw/_api/forums/pet/posts?popular=true', { mode: 'cors' })
+      .then(res => res.json())
+      .then(
+        (result) => {
+          const items = this.state.items;
+          items.push(result);
+          this.setState({
+            isLoaded: true,
+            items: items
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+
+
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      const items = this.state.items;
+      return (
+        <div>
+          {items[0].map(
+            (item) => (
+              item.media.map(
+                (a) => ( <img alt='' src={a.url} /> )
+              )
+            )
+          )
+          }
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
